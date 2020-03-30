4f7d544b448c10286e1bef5f2ad121b77b7345ef
Dragos-R <61968743+Dragos-R@users.noreply.github.com>
2020-03-30 11:42:38 +0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -7,7 +7,7 @@ template = "${file("userdata.sh")}"
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-09a5b0b7edf08843d"
+  ami           = "ami-004d3abe7e5109c79"
   instance_type = "t2.micro"
   key_name = "terraform"
   user_data = "${data.template_file.user_data.rendered}"
