04faa72cef3c3752fb273595ccc05a942528985f
Dragos-R <61968743+Dragos-R@users.noreply.github.com>
2020-03-30 13:25:11 +0300

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,21 +1,20 @@
 userdata.sh
 
 #! /bin/bash
-sudo yum  update -y
-sudo yum install httpd -y
-sudo service httpd start
-sudo sudo yum-config-manager --enable epel
-sudo yum install ansible -y
+sudo apt-get update
+sudo apt install apache2 -y
+sudo service apache2 start
+sudo apt-get install ansible -y
 echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
 echo "localhost ansible_connection=local" | sudo tee etc/ansible/hosts
 cat > install.yml << EOF
 ---
-  - name: "Installing wget"
+  - name: "Playing with mysql"
     hosts: localhost
     connection: local
     tasks:
-      - name: Install wget
-        yum: name=wget state=latest
+      - name: Install mysql
+        yum: name=mysql-server state=installed
 EOF
 sudo chmod 777 install.yml
 ansible-playbook install.yml
