35bee2922e1dae212bb558fc23cb06cb778d2e09
Dragos-R <61968743+Dragos-R@users.noreply.github.com>
2020-03-30 11:41:55 +0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -9,7 +9,7 @@ template = "${file("userdata.sh")}"
 resource "aws_instance" "web" {
   ami           = "ami-09a5b0b7edf08843d"
   instance_type = "t2.micro"
-  key_name = "Dragos-R"
+  key_name = "terraform"
   user_data = "${data.template_file.user_data.rendered}"
   tags = {
     Name = "newproject"
