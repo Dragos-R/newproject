97f182c8ccbcfd7dbd18cbc5d9c630c8e3d2474f
dragos-r <rusudragos27@gmail.com>
2020-03-30 11:19:43 +0300

    added ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "template_file" "user_data" {
+template = "${file("userdata.sh")}"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-09a5b0b7edf08843d"
+  instance_type = "t2.micro"
+  key_name = "ansible"
+  user_data = "${data.template_file.user_data.rendered}"
+  tags = {
+    Name = "HelloWorld"
+  }
+}
