45f4a12f2b4a04e2052ab5b927d2eef1c7330ea3
dragos-r <rusudragos27@gmail.com>
2020-03-30 09:21:08 +0300

    2nd commit

---
+++ userdata.sh
@@ -0,0 +1,21 @@
+userdata.sh
+
+#! /bin/bash
+sudo yum  update -y
+sudo yum install httpd -y
+sudo service httpd start
+sudo sudo yum-config-manager --enable epel
+sudo yum install ansible -y
+echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
+echo "localhost ansible_connection=local" | sudo tee etc/ansible/hosts
+cat > install.yml << EOF
+---
+  - name: "Installing wget"
+    hosts: localhost
+    connection: local
+    tasks:
+      - name: Install wget
+        yum: name=wget state=installed
+EOF
+sudo chmod 777 install.yml
+ansible-playbook install.yml
