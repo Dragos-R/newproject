e897f8891f3f8db8eb3acabc9a19aed2cecd938c
Dragos-R <61968743+Dragos-R@users.noreply.github.com>
2020-03-30 11:39:17 +0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -9,9 +9,9 @@ template = "${file("userdata.sh")}"
 resource "aws_instance" "web" {
   ami           = "ami-09a5b0b7edf08843d"
   instance_type = "t2.micro"
-  key_name = "ansible"
+  key_name = "Dragos-R"
   user_data = "${data.template_file.user_data.rendered}"
   tags = {
-    Name = "HelloWorld"
+    Name = "newproject"
   }
 }
